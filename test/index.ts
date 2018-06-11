eaf0d1d745e3ef35a2a49c1072dec22531cc2e90
Kir Belevich <kir@belevi.ch>
2018-06-11 21:44:24 +0200

    👾 test deleting symlinks

--- test/index.ts
+++ test/index.ts
@@ -2,16 +2,19 @@ import test from 'tape-promise/tape'
 import { createFsFromVolume, Volume } from 'memfs'
 import { mock, unmock } from 'mocku'
 
-test('delete directory with all files', async (t) => {
+test('delete directory with subdirectories, files and symlinks', async (t) => {
   const vol = Volume.fromJSON({
     '/test/1.md': '',
     '/test/foo/2.md': '',
     '/test/foo/bar/3.md': ''
   })
+  const fs = createFsFromVolume(vol)
+  type Symlink = (target: string, path: string, cb: (error: any) => void) => void
+  const symlink = makethen(fs.symlink as Symlink)
 
-  mock('../src/', {
-    fs: createFsFromVolume(vol)
-  })
+  await symlink('/test/foo/2.md', '/test/symlink')
+
+  mock('../src/', { fs })
 
   const { default: dleet } = await import('../src/')
 
